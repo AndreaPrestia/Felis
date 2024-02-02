5df80aff5c9533d7e826202f6d051c5c3c927ee4
Andrea Prestia <andrea@prestia.dev>
2024-02-02 15:51:17 +0100

    chore: code refactoring

--- Felis.Core/ConsumedMessage.cs
+++ Felis.Core/ConsumedMessage.cs
@@ -2,6 +2,5 @@
 
 public record ConsumedMessage(Message? Message, ConnectionId ConnectionId)
 {
-    public Guid Id { get; } = Guid.NewGuid();
     public long Timestamp { get; } = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
 }

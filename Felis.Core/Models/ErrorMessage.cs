1d69ff552a0658e2370798f012641240607feb52
Andrea Prestia <andrea@prestia.dev>
2024-05-20 10:31:08 +0200

    chore: code cleanup

--- Felis.Core/Models/ErrorMessage.cs
+++ Felis.Core/Models/ErrorMessage.cs
@@ -1,15 +1,11 @@
-﻿namespace Felis.Core.Models
+﻿namespace Felis.Core.Models;
+
+public record ErrorMessage(Guid Id, Message Message, List<ErrorMessageDetail> Errors);
+
+public record ErrorMessageDetail(ConnectionId? ConnectionId, List<ErrorDetail> Details, RetryPolicy? RetryPolicy);
+public record ErrorDetail(string? Title, string? Detail);
+
+public record ErrorMessageRequest(Guid Id, ConnectionId? ConnectionId, ErrorDetail Error, RetryPolicy? RetryPolicy)
 {
-	public record ErrorMessage(Guid Id, Message Message, List<ErrorMessageDetail> Errors)
-	{
-		public long Timestamp { get; } = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
-	}
-
-    public record ErrorMessageDetail(ConnectionId? ConnectionId, List<ErrorDetail> Details, RetryPolicy? RetryPolicy);
-	public record ErrorDetail(string? Title, string? Detail);
-
-    public record ErrorMessageRequest(Guid Id, ConnectionId? ConnectionId, ErrorDetail Error, RetryPolicy? RetryPolicy)
-    {
-        public long Timestamp { get; } = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
-    }
+    public long Timestamp { get; } = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
 }
--- Felis.Core/Models/Message.cs
+++ Felis.Core/Models/Message.cs
@@ -4,7 +4,9 @@ public record Message(Header? Header, Content? Content);
 
 public record Header(Guid Id, string Topic, long Timestamp)
 { 
-    public long Timestamp => new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
+    public Header(Guid Id, string Topic) : this(Id, Topic, new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds())
+    {
+    }
 }
 
 public record Content(string? Payload);
